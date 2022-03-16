aad57ebc0ba9f00caf865da937268b3aad0d53d4
Leela05 <leelarenu05@gmail.com>
2022-03-16 14:47:47 +0530

    Subtraction

--- src/com/Harman/Addition.java
+++ src/com/Harman/Addition.java
@@ -2,8 +2,11 @@ package com.Harman;
 
 public class Addition {
     public static void main(String[] args) {
+        Subtraction sub_obj=new Subtraction();
         int x = 3, y = 4, c;
         c = x + y;
         System.out.println(c);
+        int subresult=sub_obj.sub2numbers(x,y);
+        System.out.println(subresult);
     }
 }
--- src/com/Harman/Subtraction.java
+++ src/com/Harman/Subtraction.java
@@ -1,9 +1,9 @@
 package com.Harman;
 
-public class Calculator {
+public class Subtraction {
     public static void main(String[] args) {
-        int x=3,y=4,c;
-        c=x+y;
+        int x=5,y=4,c;
+        c=x-y;
         System.out.println(c);
     }
 }

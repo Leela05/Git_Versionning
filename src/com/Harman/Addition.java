471d3d3c0085076b69138f75cf6731cd8101fae1
Leela05 <leelarenu05@gmail.com>
2022-03-16 14:23:24 +0530

    Addition

--- src/com/Harman/Addition.java
+++ src/com/Harman/Addition.java
@@ -1,5 +1,9 @@
 package com.Harman;
 
 public class Addition {
-
+    public static void main(String[] args) {
+        int x = 3, y = 4, c;
+        c = x + y;
+        System.out.println(c);
+    }
 }
